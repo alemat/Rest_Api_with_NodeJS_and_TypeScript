5278e3219feaba93d40888e5001d2ad9b59ac32b
Alemat Gebru <alematgebru@gmail.com>
2017-03-12 00:39:44 -0800

    Delete createHouseHoldMember.js

--- server/queries/createHouseHoldMember.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var model_1 = require("../model/model");
-function createHouseHoldMember(props) {
-    return model_1.HouseHoldMemberModel.create(props);
-}
-exports.createHouseHoldMember = createHouseHoldMember;
