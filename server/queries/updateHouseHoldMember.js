d4fd6f77e02385291ef287e9848b62738e7566ae
Alemat Gebru <alematgebru@gmail.com>
2017-03-12 00:40:15 -0800

    Delete updateHouseHoldMember.js

--- server/queries/updateHouseHoldMember.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var model_1 = require("../model/model");
-function updateHouseHoldMember(id, props) {
-    return model_1.HouseHoldMemberModel.update(props, {
-        where: { id: id }
-    });
-}
-exports.updateHouseHoldMember = updateHouseHoldMember;
