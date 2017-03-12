dae2604b0956b8355db7699006bced5081b871be
Alemat Gebru <alematgebru@gmail.com>
2017-03-12 00:42:34 -0800

    Update apiGetAllHouseHolds.ts

--- server/api/apiGetAllHouseHolds.ts
+++ server/api/apiGetAllHouseHolds.ts
@@ -6,16 +6,9 @@ import {onSuccess} from "./onSuccess";
 
 export function apiGetAllHouseHolds(req:Request, res: Response){
 
-    //throw new Error('error occured');
-
      findAllHouseHolds()
-         // middleware error handling (global error handling)does not catch errors that occur here for instance
-       // .then(() => {throw new Error('error')})
         .then(_.partial(onSuccess, res))
-       //  .catch(err => res.status(500).send());
-         .catch(_.partial(onError, res, "Find all house holds failed"));//to prevent http hanging request we need to handle error in the promise chain as well by adding a catch to the promise
-
-// but we have to move the error handler into a separate file function
+         .catch(_.partial(onError, res, "Find all house holds failed"));
 }
 
 
