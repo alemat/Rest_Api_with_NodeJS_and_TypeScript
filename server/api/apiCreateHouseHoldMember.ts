b4f3c56eeaf0f2e8541db8f90b49e55ce034555e
Alemat Gebru <alematgebru@gmail.com>
2017-03-12 00:41:46 -0800

    Update apiCreateHouseHoldMember.ts

--- server/api/apiCreateHouseHoldMember.ts
+++ server/api/apiCreateHouseHoldMember.ts
@@ -14,17 +14,4 @@ export function apiCreateHouseHoldMember (req: Request, res:Response){
         .catch(_.partial(databaseErrorHandler, res))
         .catch( _.partial(onError, res, `Could not find course detail`));
 
-    /*  createHouseHoldMember (req.body)
-     .then(_.partial(onSuccess, res))
-     .catch(err => {
-
-     //throw new Error('Error occured');
-     const id = hri.random();
-     console.error("Database error occured", id, err);
-     res.status(500).json({code: 'ERR-002',
-     message: `Creation of lesson failed, error code ${id}` });
-
-     // console.error(err);
-     // res.status(500).json({code: 'ERR-002', message: err.message});
-     })*/
 }
