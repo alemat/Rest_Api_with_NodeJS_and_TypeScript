533cfe51530bae244ca928e4d2b3b6b25fd1de00
Alemat Gebru <alematgebru@gmail.com>
2017-03-12 00:39:52 -0800

    Delete deleteHouseHoldMember.js

--- server/queries/deleteHouseHoldMember.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var model_1 = require("../model/model");
-function deleteHouseHoldMember(id) {
-    return model_1.HouseHoldMemberModel.destroy({
-        where: { id: id }
-    });
-}
-exports.deleteHouseHoldMember = deleteHouseHoldMember;
